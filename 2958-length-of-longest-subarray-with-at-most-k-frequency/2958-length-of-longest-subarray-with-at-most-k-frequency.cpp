1cc2a26aeb50fd87e4d85c5729873e83948c5357
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-12-26 12:01:19 +0530

    Time: 69 ms (37.8%), Space: 149.4 MB (25.65%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -7,26 +7,53 @@ public:
 
         unordered_map<int , int> mpp;
 
-        int maxi = INT_MIN;
-
         int i = 0;
         for(int j=0; j<n; j++) {
             mpp[nums[j]]++;
-            maxi = max(maxi , mpp[nums[j]]);
 
-            while(maxi > k) {
+            while(mpp[nums[j]] > k) {
                 mpp[nums[i]]--;
-                maxi = INT_MIN;
-                for(auto it : mpp) {
-                    maxi = max(maxi , it.second);
-                }
                 i++;
             }
-
-            if(maxi <= k) {
-                ans = max(ans , (j - i + 1));
-            }
+            ans = max(ans , (j - i + 1));
         }
         return ans;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     int maxSubarrayLength(vector<int>& nums, int k) {
+//         int n = nums.size();
+
+//         int ans = 0;
+
+//         unordered_map<int , int> mpp;
+
+//         int maxi = INT_MIN;
+
+//         int i = 0;
+//         for(int j=0; j<n; j++) {
+//             mpp[nums[j]]++;
+//             maxi = max(maxi , mpp[nums[j]]);
+
+//             while(maxi > k) {
+//                 mpp[nums[i]]--;
+//                 maxi = INT_MIN;
+//                 for(auto it : mpp) {
+//                     maxi = max(maxi , it.second);
+//                 }
+//                 i++;
+//             }
+
+//             if(maxi <= k) {
+//                 ans = max(ans , (j - i + 1));
+//             }
+//         }
+//         return ans;
+//     }
+// };

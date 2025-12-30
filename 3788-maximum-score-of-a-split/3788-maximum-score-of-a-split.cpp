d49445cf1cd6ec755109c33d86a191e1cdf98989
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-12-30 22:09:09 +0530

    Time: 8 ms (56.85%), Space: 161.5 MB (10.15%) - LeetHub

---
+++ 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long maximumScore(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<long long> prefix(n);
+        prefix[0] = nums[0];
+        for(int i=1; i<n; i++) {
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+
+        vector<long long> suffix(n);
+        suffix[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--) {
+            suffix[i] = min(suffix[i+1] , (long long)nums[i]);
+        }
+
+        long long ans = LLONG_MIN;
+        for(int i=0; i<n-1; i++) {
+            ans = max(ans , (long long)(prefix[i] - suffix[i+1]));
+        }
+        return ans;
+    }
+};

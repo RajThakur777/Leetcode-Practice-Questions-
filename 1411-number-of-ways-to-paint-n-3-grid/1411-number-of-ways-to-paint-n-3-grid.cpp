3793c40453e7dcf8ebaa84980b36c6d972c1f2c5
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-01-03 23:18:53 +0530

    Time: 482 ms (6%), Space: 18.4 MB (20.33%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -1,31 +1,48 @@
 class Solution {
 public:
+     
+    vector<string> v =  {"RYG", "RGY", "RYR", "RGR", "YRG", "YGR", "YGY", "YRY", "GRY", "GYR", "GRG", "GYG"};
+
     const int mod = 1e9 + 7;
 
-    int solve(int i , int n , int p1 , int p2 , int p3 , vector<vector<vector<vector<int>>>> &dp) {
-        if(i == n) return 1;
+    int solve(int row , int prev , int n , vector<vector<int>> &dp) {
+        if(row == n) return 1;
 
-        if(dp[i][p1][p2][p3] != -1) return dp[i][p1][p2][p3];
+        if(dp[row][prev] != -1) {
+            return dp[row][prev];
+        }
 
         int ans = 0;
 
-        for(int c1=1; c1<=3; c1++) {
-            if(c1 == p1) continue;
-            for(int c2=1; c2<=3; c2++) {
-                if(c2 == p2 || c2 == c1) continue;
-                for(int c3=1; c3<=3; c3++) {
-                    if(c3 == p3 || c3 == c2) continue;
+        for(int i=0; i<12; i++) {
+            if(i == prev) continue;
 
-                    ans = (ans + solve(i+1 , n , c1 , c2 , c3 , dp)) % mod;
+            string s1 = v[i];
+            string s2 = v[prev];
+
+            bool f = true;
+            for(int j=0; j<3; j++) {
+                if(s1[j] == s2[j]) {
+                    f = false;
+                    break;
                 }
             }
+
+            if(f) {
+                ans = (ans + solve(row + 1 , i , n , dp)) % mod;
+            }
         }
-        return dp[i][p1][p2][p3] = (ans % mod);
+        return dp[row][prev] = ans;
     }
 
-    int numOfWays(int n) { 
-        vector<vector<vector<vector<int>>>> dp(n , vector<vector<vector<int>>>(4 , vector<vector<int>>(4 , vector<int>(4 , -1))));
+    int numOfWays(int n) {
+        int ans = 0;
 
-        return solve(0 , n , 0 , 0 , 0 , dp);     
+        vector<vector<int>> dp(n , vector<int>(12 , -1));
+
+        for(int i=0; i<12; i++) {
+            ans = (ans + solve(1 , i , n , dp)) % mod;
+        }
+        return ans;
     }
 };

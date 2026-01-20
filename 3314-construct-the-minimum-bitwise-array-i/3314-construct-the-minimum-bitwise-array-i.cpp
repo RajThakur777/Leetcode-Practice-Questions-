4eafc4ecdcefe4051db06fb7fb58edc17a35ad00
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-01-20 07:55:23 +0530

    Time: 0 ms (100%), Space: 25.1 MB (78.7%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=0; i<n; i++) {
             int val = nums[i];
             int v = -1;
-            for(int j=1; j<999; j++) {
+            for(int j=1; j<=999; j++) {
                 if((j | (j + 1)) == val) {
                     v = j;
                     break;

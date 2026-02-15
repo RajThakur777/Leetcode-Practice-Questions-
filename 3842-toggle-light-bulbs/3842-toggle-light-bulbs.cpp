9356bfebecfb6d84a5cfb1e6c0773d2c440d9e5b
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-02-15 09:37:50 +0530

    Time: 0 ms (100%), Space: 34.7 MB (34.95%) - LeetHub

---
+++ 3842-toggle-light-bulbs/3842-toggle-light-bulbs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> toggleLightBulbs(vector<int>& bulbs) {
+        int n = bulbs.size();
+
+        map<int , int> mpp;
+        for(int i=0; i<n; i++) {
+            mpp[bulbs[i]]++;
+        }
+
+        vector<int> ans;
+        for(auto it : mpp) {
+            if(it.second % 2 != 0) {
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};

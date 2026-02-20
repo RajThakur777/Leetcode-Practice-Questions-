1faec774fa2c4e5ba258cabe9ba2bb573d99101b
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-02-20 23:07:36 +0530

    Time: 6 ms (52.58%), Space: 20.1 MB (36.23%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = prerequisites.size();
+
+        unordered_map<int , vector<int>> mpp;
+        vector<int> indegree(numCourses);
+        for(int i=0; i<n; i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+
+            mpp[u].push_back(v);
+            indegree[v]++;
+        }
+
+        queue<int> q;
+
+        for(int i=0; i<numCourses; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            ans.push_back(node);
+
+            for(auto it : mpp[node]) {
+                indegree[it]--;
+
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        return (ans.size() == numCourses);
+    }
+};

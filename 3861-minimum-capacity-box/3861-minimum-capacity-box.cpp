2e77d8dd901ab49e2912ef8c29a94b916e8c83b6
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-03-09 13:37:15 +0530

    Time: 0 ms (100%), Space: 23.3 MB (0%) - LeetHub

--- 3861-minimum-capacity-box/3861-minimum-capacity-box.cpp
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.cpp
@@ -4,18 +4,14 @@ public:
         int n = capacity.size();
 
         int ans = INT_MAX;
+        int idx = -1;
 
         for(int i=0; i<n; i++) {
-            if(capacity[i] >= itemSize) {
+            if(capacity[i] >= itemSize && capacity[i] < ans) {
                 ans = min(ans , capacity[i]);
+                idx = i;
             }
         }
-
-        for(int i=0; i<n; i++) {
-            if(capacity[i] == ans) {
-                return i;
-            }
-        }
-        return -1;
+        return idx;
     }
 };

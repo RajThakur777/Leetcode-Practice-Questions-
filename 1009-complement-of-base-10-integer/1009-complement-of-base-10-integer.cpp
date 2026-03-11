79dc108881447491ebc79f221f3ab361a58b28a5
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-03-11 08:10:51 +0530

    Time: 0 ms (100%), Space: 7.9 MB (59.24%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+
+        if(n == 0) return 1;
+
+        int bits = 31 - __builtin_clz(n);
+
+        string str;
+        for(int i = 0; i <= bits; i++) {
+            if((n & (1 << i)) > 0) {
+                str += '0';
+            }
+            else {
+                str += '1';
+            }
+        }
+
+        reverse(str.begin() , str.end());
+
+        int idx = 0;
+        int ans = 0;
+
+        for(int i = str.size() - 1; i >= 0; i--) {
+            if(str[i] == '1') {
+                ans += pow(2 , idx);
+            }
+            idx++;
+        }
+
+        return ans;
+    }
+};

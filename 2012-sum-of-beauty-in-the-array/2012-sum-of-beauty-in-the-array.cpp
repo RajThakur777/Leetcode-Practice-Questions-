4f6461ad7ccb701b7597ca578f3282e7afcde5fd
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-03-26 18:46:49 +0530

    Time: 7 ms (44.24%), Space: 92.9 MB (64.31%) - LeetHub

---
+++ 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int sumOfBeauties(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> prefix(n);
+        prefix[0] = nums[0];
+        for(int i=1; i<n; i++) {
+            prefix[i] = max(prefix[i-1] , nums[i]);
+        }
+
+        vector<int> suffix(n);
+        suffix[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--) {
+            suffix[i] = min(suffix[i+1] , nums[i]);
+        }
+
+        int ans = 0;
+
+        for(int i=1; i<n-1; i++) {
+            int ele = nums[i];
+
+            if(nums[i] > prefix[i-1] && nums[i] < suffix[i+1]) {
+                ans += 2;
+            }
+            else if(nums[i] > nums[i-1] && nums[i] < nums[i+1]) {
+                ans += 1;
+            }
+            else {
+                ans += 0;
+            }
+        }
+        return ans;
+    }
+};

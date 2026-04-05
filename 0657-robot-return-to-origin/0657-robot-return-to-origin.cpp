2845d56958ac791f7f4bea38f39ef1886eb6c3d0
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-04-05 10:39:57 +0530

    Time: 3 ms (46.4%), Space: 10.3 MB (47.26%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+
+        for(int i=0; i<moves.size(); i++) {
+            if(moves[i] == 'L') {
+                x--;
+            }
+            else if(moves[i] == 'R') {
+                x++;
+            }
+            else if(moves[i] == 'U') {
+                y++;
+            }
+            else {
+                y--;
+            }
+        }
+        return (x == 0 && y == 0);
+    }
+};

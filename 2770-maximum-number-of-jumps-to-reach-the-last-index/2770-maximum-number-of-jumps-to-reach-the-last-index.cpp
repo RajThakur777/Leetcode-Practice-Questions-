7565eecc53f3000fcc017a3ea5caad490a521b68
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-05-10 19:00:22 +0530

    Time: 19 ms (62.42%), Space: 67.5 MB (95.34%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,14 +1,14 @@
 class Solution {
-public:
+public: 
     int dp[1001];
 
     int solve(int idx , vector<int> &nums , int target) {
-        if(idx >= nums.size()) {
-            return -1;
-        }
-        
-        if(idx == nums.size()-1) {
+        if(idx == nums.size() - 1) {
             return 0;
+        }
+
+        if(idx >= nums.size()) {
+            return INT_MIN;
         }
 
         if(dp[idx] != -1) {
@@ -16,15 +16,18 @@ public:
         }
 
         int ans = INT_MIN;
-        for(int i=idx+1; i<nums.size(); i++) {
-            if((abs(nums[i] - nums[idx]) <= target) && (abs(nums[i] - nums[idx]) >= -target)) {
-                ans = max(ans , 1 + solve(i , nums , target));
+
+        for(int j=idx+1; j<nums.size(); j++) {
+            if((nums[j] - nums[idx]) >= -target && (nums[j] - nums[idx]) <= target) {
+                ans = max(ans , 1 + solve(j , nums , target));
             }
         }
         return dp[idx] = ans;
     }
-   
+
     int maximumJumps(vector<int>& nums, int target) {
+        int n = nums.size();
+
         memset(dp , -1 , sizeof(dp));
 
         int ans = solve(0 , nums , target);
@@ -32,6 +35,7 @@ public:
         if(ans < 0) {
             return -1;
         }
+
         return ans;
     }
 };

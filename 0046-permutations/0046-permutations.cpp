b24553d82e9821860bece3604beb5c9f715c6963
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-06-18 07:25:45 +0530

    Time: 0 ms (100%), Space: 10.9 MB (27.4%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
 
-    void solve(int idx , vector<int> &nums , vector<int> &res , vector<vector<int>> &ans , set<int> &st) {
-        if(idx == nums.size()) {
+    void solve(vector<int> &nums , vector<int> &res , vector<vector<int>> &ans , set<int> &st) {
+        if(res.size() == nums.size()) {
             ans.push_back(res);
             return;
         }
 
         for(int i=0; i<nums.size(); i++) {
-            if(st.find(nums[i]) != st.end()) continue;
+            if(st.find(nums[i]) == st.end()) {
+                st.insert(nums[i]);
+                res.push_back(nums[i]);
 
-            st.insert(nums[i]);
-            res.push_back(nums[i]);
+                solve(nums , res , ans , st);
 
-            solve(idx+1 , nums , res , ans , st);
-
-            st.erase(nums[i]);
-            res.pop_back();
+                st.erase(nums[i]);
+                res.pop_back();
+            }
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> res;
         vector<vector<int>> ans;
+
+        vector<int> res;
 
         set<int> st;
 
-        solve(0 , nums , res , ans , st);
+        solve(nums , res , ans , st);
 
         return ans;
     }

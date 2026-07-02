17d0b5cdc5d687e7e68081ab998886bac690d52b
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-07-02 09:20:17 +0530

    Time: 0 ms (100%), Space: 9.8 MB (49.63%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+
+        if(m != n) {
+            return false;
+        }
+
+        vector<int> freq1(26 , 0);
+        vector<int> freq2(26 , 0);
+
+        for(int i=0; i<s.size(); i++) {
+            freq1[s[i] - 'a']++;
+            freq2[t[i] - 'a']++;
+        }
+
+        for(int i=0; i<26; i++) {
+            if(freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};

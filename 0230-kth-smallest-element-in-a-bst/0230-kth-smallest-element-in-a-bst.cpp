49bcfc4087aedac159e24cf867d9b848c9de2b2e
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-07-12 09:44:23 +0530

    Time: 0 ms (100%), Space: 24.4 MB (40.87%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,45 +9,22 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-// //TC->O(N);
-// //SC->O(N);
-// class Solution {
-// public:
-
-//     void solve(TreeNode* root , vector<int> &ans) {
-//         if(root == nullptr) return;
-
-//         solve(root->left , ans);
-//         ans.push_back(root->val);
-//         solve(root->right , ans);
-//     }
-
-//     int kthSmallest(TreeNode* root, int k) {
-//         vector<int> ans;
-//         solve(root , ans);
-
-//         return ans[k-1];
-//     }
-// };
-
-
-
-
-//TC->O(N);
-//SC->O(1);
 class Solution {
 public:
     int cnt = 0;
-    int val = 0;
+    int ans = 0;
+
     void solve(TreeNode* root , int k) {
-        if(root == nullptr) return;
+        if(!root) {
+            return;
+        }
 
         solve(root->left , k);
 
         cnt++;
+        
         if(cnt == k) {
-            val = root->val;
+            ans = root->val;
             return;
         }
 
@@ -57,6 +34,6 @@ public:
     int kthSmallest(TreeNode* root, int k) {
         solve(root , k);
 
-        return val;
+        return ans;
     }
 };

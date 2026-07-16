b60ee006c4ee64831dc124660e33d4a8a18c7cc4
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-07-16 09:41:51 +0530

    Time: 88 ms (12.6%), Space: 171.9 MB (11.21%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -3,30 +3,19 @@ public:
     long long gcdSum(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> maxi(n);
-        maxi[0] = nums[0];
+        vector<long long> m(n);
+        vector<long long> prefix(n);
 
+        m[0] = nums[0];
         for(int i=1; i<n; i++) {
-            maxi[i] = max(maxi[i-1] , nums[i]);
+            m[i] = max(m[i-1] , (long long)nums[i]);
         }
 
-        vector<int> prefix(n);
-
         for(int i=0; i<n; i++) {
-            prefix[i] = __gcd(nums[i] , maxi[i]);
+            prefix[i] = __gcd((long long)nums[i] , m[i]);
         }
 
         sort(prefix.begin() , prefix.end());
-
-        set<int> st;
-        for(auto it : prefix) {
-            st.insert(it);
-        }
-
-        vector<int> arr;
-        for(auto it : st) {
-            arr.push_back(it);
-        }
 
         int i = 0;
         int j = n-1;
@@ -34,10 +23,11 @@ public:
         long long sum = 0;
 
         while(i < j) {
-            sum += (__gcd(prefix[i] , prefix[j]));
+            sum += __gcd(prefix[i] , prefix[j]);
             i++;
             j--;
         }
+
         return sum;
     }
 };

92ed7e8eafd8c4a42e79e677d2b8e9189aacc384
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-07-17 10:02:45 +0530

    Time: 0 ms (100%), Space: 17.4 MB (58.74%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-
-    bool dfs(int node , vector<int> &color , unordered_map<int , vector<int>> &mpp , int c) {
+    bool dfs(int node , vector<int> &color , vector<vector<int>> &graph , int c) {
         color[node] = c;
 
-        for(auto it : mpp[node]) {
+        for(auto it : graph[node]) {
             if(color[it] == -1) {
-                if(dfs(it , color , mpp , 1-c) == true) {
-                    return true;
+                if(dfs(it , color , graph , 1-c) == false) {
+                    return false;
                 }
             }
-            else if(color[it] == color[node]) {
-                return true;
+            else if(color[it] == c) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
 
-        unordered_map<int , vector<int>> mpp;
-
-        for(int i=0; i<n; i++) {
-            for(auto it : graph[i]) {
-                mpp[i].push_back(it);
-                mpp[it].push_back(i);
-            }
-        }
-
         vector<int> color(n , -1);
+
+        int c = 1;
 
         for(int i=0; i<n; i++) {
             if(color[i] == -1) {
-                if(dfs(i , color , mpp , 1) == true) {
+                if(dfs(i , color , graph , c) == false) {
                     return false;
                 }
             }

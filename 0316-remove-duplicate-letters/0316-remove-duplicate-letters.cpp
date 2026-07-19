f4e5b2ff29022dd8a8c500384feaa114d489f56e
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-07-19 11:55:26 +0530

    Time: 0 ms (100%), Space: 9.2 MB (68.83%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n = s.size();
+
+        vector<bool> vis(26 , false);
+
+        vector<int> last_occ(26);
+
+        for(int i=0; i<n; i++) {
+            char ch = s[i];
+
+            last_occ[ch - 'a'] = i;
+        }
+
+        string ans;
+
+        for(int i=0; i<n; i++) {
+            if(vis[s[i] - 'a']) {
+                continue;
+            }
+
+            while(ans.size() > 0 && ans.back() > s[i] && last_occ[ans.back() - 'a'] > i) {
+                vis[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+
+            ans += s[i];
+            vis[s[i] - 'a'] = true;
+        }
+
+        return ans;
+    }
+};

d8beae9138b1e62d8858a1f3e34276cfd372d6f3
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-07-21 10:29:24 +0530

    Time: 0 ms (100%), Space: 97.3 MB (61.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,15 +4,16 @@ public:
         int n = prices.size();
 
         int ans = 0;
-
         int buy = prices[0];
 
         for(int i=1; i<n; i++) {
             if(prices[i] > buy) {
                 ans = max(ans , prices[i] - buy);
             }
+
             buy = min(buy , prices[i]);
         }
+
         return ans;
     }
 };

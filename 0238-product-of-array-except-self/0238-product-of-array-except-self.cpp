4020b0027b9a05e6af911ccd9e249aca75678e19
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-07-21 11:03:25 +0530

    Time: 3 ms (37.62%), Space: 44.2 MB (5.09%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,32 +3,32 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> prefix(n);
-        vector<int> suffix(n);
-
+        vector<long long> prefix(n);
         prefix[0] = nums[0];
-        suffix[n-1] = nums[n-1];
 
         for(int i=1; i<n; i++) {
-            prefix[i] = (prefix[i-1] * nums[i]);
+            prefix[i] = (prefix[i-1] * 1LL * nums[i]);
         }
 
+        vector<long long> suffix(n);
+        suffix[n-1] = nums[n-1];
+
         for(int i=n-2; i>=0; i--) {
-            suffix[i] = (suffix[i+1] * nums[i]);
+            suffix[i] = (suffix[i+1] * 1LL * nums[i]);
         }
 
         vector<int> ans(n);
 
         for(int i=0; i<n; i++) {
             if(i == 0) {
-                ans[i] = suffix[i+1];
+                ans[i] = (suffix[i+1]);
             }
             else if(i == n-1) {
                 ans[i] = prefix[i-1];
             }
             else {
-                int result = (prefix[i-1] * suffix[i+1]);
-                ans[i] = result;
+                int val = (prefix[i-1] * 1LL * suffix[i+1]);
+                ans[i] = val;
             }
         }
         return ans;

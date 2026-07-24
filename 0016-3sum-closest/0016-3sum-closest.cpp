1ac546b217804e57de16af1427d1e4245a9e90c6
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-07-24 11:14:19 +0530

    Time: 11 ms (59.51%), Space: 14 MB (81.81%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -6,38 +6,29 @@ public:
         sort(nums.begin() , nums.end());
 
         int ans = INT_MAX;
-
         int res = 0;
 
         for(int j=1; j<n-1; j++) {
             int i = 0;
             int k = n-1;
 
-            bool f = false;
-
             while(i < j && k > j) {
                 int sum = nums[i] + nums[j] + nums[k];
 
-                if(abs(sum - target) < ans) {
-                    ans = abs(sum - target);
+                if(ans > abs(target - sum)) {
+                    ans = abs(target - sum);
                     res = sum;
                 }
 
-                if(sum > target) {
-                    k--;
-                }
-                else if(sum < target) {
+                if(sum < target) {
                     i++;
                 }
-                else if(sum == target){
-                    ans = 0;
-                    res = sum;
-                    f = true;
-                    break;
+                else if(sum > target){
+                    k--;
                 }
-            }
-            if(f) {
-                break;
+                else {
+                    return sum;
+                }
             }
         }
         return res;

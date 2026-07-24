68f13aa2b8fe866645fa80732381267e8d338f78
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-07-24 13:32:27 +0530

    Time: 0 ms (100%), Space: 50 MB (5.2%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    int maxScore(vector<int>& cardPoints, int k) {
+    int maxScore(vector<int>& cardPoints, int k) {  
         int n = cardPoints.size();
 
-        int sum = 0;
-        for(int i=0; i<k; i++) {
-            sum += cardPoints[i];
+        vector<int> prefix(n);
+        prefix[0] = cardPoints[0];
+
+        for(int i=1; i<n; i++) {
+            prefix[i] = prefix[i-1] + cardPoints[i];
+        } 
+
+        vector<int> suffix(n);
+        suffix[n-1] = cardPoints[n-1];
+
+        for(int i=n-2; i>=0; i--) {
+            suffix[i] = suffix[i+1] + cardPoints[i];
         }
 
-        int ans = sum;
-        int left = k-1;
-        int right = n-1;
+        int ans = 0;
 
-        while(left >= 0) {
-            sum -= cardPoints[left];
-            sum += cardPoints[right];
-            left--;
-            right--;
-            ans = max(ans , sum);
+        for(int len=0; len<=k; len++) {
+            int l = len;
+            int r = k - l;
+
+            if(len == 0) {
+                ans = max(ans , suffix[n - k]);
+            }
+            else if(len == k) {
+                ans = max(ans , prefix[k - 1]);
+            }
+            else {
+                ans = max(ans , prefix[l - 1] + suffix[n - r]);
+            }
         }
+
         return ans;
     }
 };

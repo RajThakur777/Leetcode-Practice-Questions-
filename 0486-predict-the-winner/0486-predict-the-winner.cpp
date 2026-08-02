11823da61dc15bd2eec0750d54a063377290511a
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-08-02 16:11:33 +0530

    Time: 2 ms (43.99%), Space: 10.5 MB (9.22%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
+    int dp[21][21];
 
     int solve(int i , int j , vector<int> &nums) {
         if(i > j) {
             return 0;
         }
 
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
         int op1 = nums[i] + min({solve(i+2 , j , nums) , solve(i+1 , j-1 , nums)});
 
         int op2 = nums[j] + min({solve(i , j-2 , nums) , solve(i+1 , j-1 , nums)});
 
-        return max({op1 , op2});
+        return dp[i][j] = max({op1 , op2});
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
+
+        memset(dp , -1 , sizeof(dp));
 
         int total = 0;
         for(auto x : nums) {

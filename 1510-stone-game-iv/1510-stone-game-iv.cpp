54ac4fdfef684044d544f4c8fc49a4bf6af7c0f3
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-08-10 09:01:15 +0530

    Time: 43 ms (57.44%), Space: 15.4 MB (49.71%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int dp[100001];
+
+    bool solve(int n) {
+        if(n == 0) {
+            return false;
+        }
+
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+
+        bool ans = false;
+
+        for(int i=1; i*i<=n; i++) {
+            if((solve(n - (i * i))) == false) {
+                ans = true;
+                break;
+            }
+        }
+
+        return dp[n] = ans;
+    }
+
+    bool winnerSquareGame(int n) {
+        memset(dp , -1 , sizeof(dp));
+        
+        return solve(n);
+    }
+};

cedd604d068b2d9581ab561240e341a9620fff39
Raj Girdharsingh Thakur <traj15019@gmail.com>
2026-08-11 20:46:26 +0530

    Time: 2064 ms (5.08%), Space: 244.2 MB (6.34%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
@@ -3,37 +3,32 @@ public:
     int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
         int n = s.size();
 
-        int k = maxLetters;
-
         map<string , int> mpp1;
-        map<char , int> mpp2;
 
-        int i = 0;
-        for(int j=0; j<n; j++) {
-            mpp2[s[j]]++;
+        for(int k=minSize; k<=maxSize; k++) {
+            int len = k;
 
-            while((j - i + 1) > maxSize || mpp2.size() > k) {
-                mpp2[s[i]]--;
-                if(mpp2[s[i]] == 0) {
-                    mpp2.erase(s[i]);
-                }
-                i++;
-            }
+            map<char , int> mpp2;
 
-            while((j - i + 1) >= minSize && mpp2.size() <= k) {
-                string str = s.substr(i , (j - i + 1));
+            int i = 0;
+            for(int j=0; j<n; j++) {
+                mpp2[s[j]]++;
 
-                mpp1[str]++;
-
-                mpp2[s[i]]--;
-
-                if(mpp2[s[i]] == 0) {
-                    mpp2.erase(s[i]);
+                while((j - i + 1) > len) {
+                    mpp2[s[i]]--;
+                    if(mpp2[s[i]] == 0) {
+                        mpp2.erase(s[i]);
+                    }
+                    i++;
                 }
 
-                i++;
+                if((j - i + 1) == len && mpp2.size() <= maxLetters) {
+                    string str = s.substr(i , (j - i + 1));
+
+                    mpp1[str]++;
+                }
             }
-        }  
+        }
 
         int ans = 0;
 
